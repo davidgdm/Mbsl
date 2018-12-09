30ca0493d06ed07c8b11da579dd3ea1ecd06c672
david martinez <germandavidmartinez@gmail.com>
2018-12-09 01:13:37 +0100

    UPdating connection

--- Code/Mig_daily_tracker/01_Trn_DailyMigration-1.91.py
+++ Code/Mig_daily_tracker/01_Trn_DailyMigration-1.91.py
@@ -12,6 +12,11 @@ import pyodbc
 params = urllib.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=mbslbiserver.database.windows.net;DATABASE=mbsldwh_dev;UID=Reports;PWD=mbsl1234!")
 engineDWH = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
+cnxn_dev = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=mbslbiserver.database.windows.net;'
+                      'Database=mbsldwh_dev;'
+                      'UID=Reports;'
+                      'PWD=mbsl1234!')
 
 #Uploading last day into DWH
 df_Migration = pd.read_sql_query("""select * from Migration_agg where snapshot_at =(select max(snapshot_at)from Migration_agg)""", con=cnxn_dev)
