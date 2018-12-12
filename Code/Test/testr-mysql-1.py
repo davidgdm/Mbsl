8f1fabbb07c15f06be7aecbb48e8c646ac6508e5
david martinez <germandavidmartinez@gmail.com>
2018-12-12 09:22:57 +0100

    test_mysql

---
+++ Code/Test/testr-mysql-1.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import mysql.connector as sql
+from sqlalchemy import create_engine, text
+from sqlalchemy import delete
+from datetime import datetime
+import time
+import urllib
+
+
+conSolar = sql.connect(user='david',password='ItJubSheg6',host='kaa.plugintheworld.com',database='solarhub_production')
+
+
+df_la = pd.read_sql_query("""SELECT *
+FROM loan_accounts  limit 10;""", con=conSolar)
+
+print(df_la)
