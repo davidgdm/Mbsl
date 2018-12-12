47f7446697e18c8bc1265daa70f92511fa469a90
david martinez <germandavidmartinez@gmail.com>
2018-12-12 12:55:22 +0100

    indented

--- Code/Test/test-upload-bcp-1.py
+++ Code/Test/test-upload-bcp-1.py
@@ -43,50 +43,7 @@ df_dailysnapshot = pd.read_sql_query("""SELECT la.id as loan_account_id,
     WHERE la.risk_category IS NOT NULL
     AND la.state NOT IN('paid_off', 'defaulted', 'canceled')
     GROUP BY la.id
-    ORDER BY la.id;""", con=conSolar)
+    ORDER BY la.id limit 10;""", con=conSolar)
 
-#cnxn_dev =pyodbc.connect(server='localhost', user='sa', password='Admin1234.,', database='mbsol')
-
-cnxn_dev=pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                      'Server=mbslbiserver.database.windows.net;'
-                      'Database=mbsldwh_dev;'
-                      'UID=Reports;'
-                      'PWD=mbsl1234!')
-
-                      
-
-
-tuples = list(zip(df_dailysnapshot["loan_account_id"],df_dailysnapshot["loan_portfolio_id"],df_dailysnapshot["currency"],df_dailysnapshot["lfo_name"],df_dailysnapshot["state"],df_dailysnapshot["risk_category"],df_dailysnapshot["par_days"],df_dailysnapshot["handover_at"],df_dailysnapshot["outstanding_amount"],df_dailysnapshot["snapshot_at"]))
-
-cursor = cnxn_dev.cursor()
-
-query="""Insert into [Extr_Dailysnapshot] ([loan_account_id]
-      ,[loan_portfolio_id]
-      ,[currency]
-      ,[lfo_name]
-      ,[state]
-      ,[risk_category]
-      ,[par_days]
-      ,[handover_at]
-      ,[outstanding_amount]
-      ,[snapshot_at]) values(?,?,?,?,?,?,?,?,?,?) """
-
-start = time.time()
-
-cursor.executemany(query,tuples)
-cnxn_dev.commit()
-
-end = time.time()
-print(end - start)
-
-cursor.close
-cnxn_dev.close()
-
-#params = urllib.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=mbsol;UID=sa;PWD=Admin1234.,")
-params = urllib.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=mbslbiserver.database.windows.net;DATABASE=mbsldwh_dev;UID=Reports;PWD=mbsl1234!")
-engineMSlocal = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
-
-start = time.time()
-df_dailysnapshot.to_sql('Extr_Dailysnapshot',engineMSlocal,if_exists='replace',index=False)
-end = time.time()
-print(end - start)
+df_dailysnapshot.to_csv('/home/dwh/ETL/mbsl/Code/Test/daily.csv')
+#df_dailysnapshot.to_csv('c:/daily.csv')
