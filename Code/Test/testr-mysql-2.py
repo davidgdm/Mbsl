02ec8df4a62d46542ae4739941954c64b43a5641
david martinez <germandavidmartinez@gmail.com>
2018-12-12 14:40:00 +0100

    terminal commands from python

---
+++ Code/Test/testr-mysql-2.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import mysql.connector as sql
+from sqlalchemy import create_engine, text
+from sqlalchemy import delete
+from datetime import datetime
+import time
+import urllib
+import os
+
+os.system("echo 'hello world'")
+
+
+#conSolar = sql.connect(user='david',password='ItJubSheg6',host='kaa.plugintheworld.com',database='solarhub_production', port='3306')
+#conSolar = sql.connect(user='mobisol_data_warehouse',password='mydLalm8EjimLojOd3',host='127.0.1.1',database='solarhub_production',port='3306')
+
+#df_la = pd.read_sql_query("""SELECT *
+#FROM loan_accounts  limit 10;""", con=conSolar)
+
+#print(df_la)
