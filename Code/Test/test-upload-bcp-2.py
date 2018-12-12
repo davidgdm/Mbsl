953de0fd1a96577870a00e3e1ef2171d50bb46b7
david martinez <germandavidmartinez@gmail.com>
2018-12-12 15:23:02 +0100

    with print

--- Code/Test/test-upload-bcp-2.py
+++ Code/Test/test-upload-bcp-2.py
@@ -13,7 +13,7 @@ import os
 #os.system("systemctl --mobisol_data_warehouse restart db-ssh.service")
 
 conSolar = sql.connect(user='mobisol_data_warehouse',password='mydLalm8EjimLojOd3',host='127.0.1.1',database='solarhub_production',port='3306')
-
+print('connection established')
 
 #extraction of WC
 df_dailysnapshot = pd.read_sql_query("""SELECT la.id as loan_account_id,
@@ -47,7 +47,10 @@ df_dailysnapshot = pd.read_sql_query("""SELECT la.id as loan_account_id,
     GROUP BY la.id
     ORDER BY la.id limit 50;""", con=conSolar)
 
+print ('df created')
+
 df_dailysnapshot.to_csv('/home/dwh/ETL/mbsl/Code/Test/daily.csv', index=False)
 #df_dailysnapshot.to_csv('C:/testETL/daily.csv', index=False)
+print('csv saved')
 
 os.system("""bcp Extr_Dailysnapshot in "/home/dwh/ETL/mbsl/Code/Test/daily.csv" -S mbslbiserver.database.windows.net -d mbsldwh_dev -U Reports -P mbsl1234! -q -c -t ,""")
