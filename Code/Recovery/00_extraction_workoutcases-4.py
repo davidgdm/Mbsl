32733b279259d0512927d1fb388687a361375b69
david martinez <germandavidmartinez@gmail.com>
2018-12-13 13:03:28 +0100

    payments csv

--- Code/Recovery/00_extraction_workoutcases-4.py
+++ Code/Recovery/00_extraction_workoutcases-4.py
@@ -11,7 +11,6 @@ import time
 import pymssql
 import os
 
-
 #------------------------EXTRACTION---------------------------------
 os.system("sudo systemctl restart db-ssh")
 print("service restarted")
@@ -34,10 +33,8 @@ inner join hubs on c.hub_id=hubs.id
 where lwc.could_reach_customer=1 and lwc.activity_category_id IN ("5252","5253") and lwc.closed_at >= ( CURDATE() - INTERVAL 3 DAY )
 group by lwc.id;""", con=conSolar)
 
-
 df_workcases.to_csv('/home/dwh/ETL/mbsl/Code/wc.csv', index=False)
-
-print('csv saved')
+print('csv WC saved')
 
 cnxn_dev = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                       'Server=mbslbiserver.database.windows.net;'
@@ -51,3 +48,29 @@ cnxn_dev.commit()
 print('truncate Extr_workcases finished')
 
 os.system("""bcp Extr_workcases in "/home/dwh/ETL/mbsl/Code/wc.csv" -S mbslbiserver.database.windows.net -d mbsldwh_dev -U Reports -P mbsl1234! -q -c -t ,""")
+
+#payments extraction 
+print('payments MySQl query started')
+df_payments = pd.read_sql_query("""SELECT pa.loan_portfolio_id, convert(p.transaction_at,date) as transaction_at, sum(p.amount_subunit)/100 as "Payment_Amount"
+FROM payments p inner join payment_accounts pa on p.payment_account_id=pa.id
+where convert(transaction_at,date)>=2018-08-01
+and  pa.loan_portfolio_id is not null
+group by pa.loan_portfolio_id, convert(p.transaction_at,date);""", con=conSolar)
+
+print('payments MySQl query finished')
+
+df_payments.to_csv('/home/dwh/ETL/mbsl/Code/payments.csv', index=False)
+print('csv payments saved')
+
+cnxn_dev = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=mbslbiserver.database.windows.net;'
+                      'Database=mbsldwh_dev;'
+                      'UID=Reports;'
+                      'PWD=mbsl1234!')
+cursor = cnxn_dev.cursor()
+querystring ="""truncate table Extr_payments;"""
+cursor.execute(querystring)
+cnxn_dev.commit()
+print('truncate Extr_payments finished')
+
+os.system("""bcp Extr_payments in "/home/dwh/ETL/mbsl/Code/payments.csv" -S mbslbiserver.database.windows.net -d mbsldwh_dev -U Reports -P mbsl1234! -q -c -t ,""")
