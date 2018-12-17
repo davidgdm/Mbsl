1ba877f7334495bad970cacc73595dcf365300ce
david martinez <germandavidmartinez@gmail.com>
2018-12-17 16:48:22 +0100

    bcp4

---
+++ Code/Test/test-upload-bcp-4.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import mysql.connector as sql
+from sqlalchemy import create_engine, text
+from sqlalchemy import delete
+import datetime
+import time
+import urllib
+import pyodbc
+import time
+import pymssql
+import subprocess
+
+
+
+
+print('Process has started at:')
+print(datetime.datetime.now())
+
+subprocess.Popen("""bcp Extr_bcp in "/home/dwh/ETL/mbsl/Code/daily.csv" -S mbslbiserver.database.windows.net -d mbsldwh_dev -U Reports -P mbsl1234! -q -c -t ,""")
+print('\n\nProcess has finished at:')
+print(datetime.datetime.now())
+print('-------------------------------')
